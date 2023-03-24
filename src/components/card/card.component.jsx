1251152282548f9e7951838baff3a80a08ce0b96
Brian Hopkins <brianhh1230@gmail.com>
2023-03-24 08:26:26 -0400

    feat(api): updating public api

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,15 +1,15 @@
 import "./card.styles.css";
 
 const Card = ({ subscriber }) => {
-  const { id, user_name, is_gifted } = subscriber;
+  const { id, user_name, is_gifted, profile_photo } = subscriber;
   return (
     <div key={id} className="card-container">
       <img
         alt={`subscriber ${user_name}`}
-        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        src={profile_photo}
       />
       <h2 className="card-header">{user_name}</h2>
-      Is Gift: {is_gifted + ''}
+      Gifted: {is_gifted + ''}
     </div>
   );
 };
