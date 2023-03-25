2bb784992e4ed64f7aa00e4adeccf82e7a2a1dfd
Brian Hopkins <brianhh1230@gmail.com>
2023-03-24 21:30:26 -0400

    fix(sytling): updating styling on cards and adding in founder part

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,15 +1,15 @@
 import "./card.styles.css";
 
 const Card = ({ subscriber }) => {
-  const { id, user_name, is_gifted, profile_photo } = subscriber;
+  const { id, user_name, founder, profile_photo } = subscriber;
+  const isFounder = founder === "yes";
+  const founderClass = isFounder ? "founder" : "";
+
   return (
-    <div key={id} className="card-container">
-      <img
-        alt={`subscriber ${user_name}`}
-        src={profile_photo}
-      />
+    <div key={id} className={`card-container ${founderClass}`}>
+      <img alt={`subscriber ${user_name}`} src={profile_photo} />
       <h2 className="card-header">{user_name}</h2>
-      Gifted: {is_gifted + ''}
+      {isFounder && <p className="founder-text">Founder</p>}
     </div>
   );
 };
