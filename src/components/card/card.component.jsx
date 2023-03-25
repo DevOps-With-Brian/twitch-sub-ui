c59e5fc40ad5de3ec4ce615c751f92e6819383bd
Brian Hopkins <brianhh1230@gmail.com>
2023-03-24 22:42:21 -0400

    feat(rgb): making rgb cards for founders

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,15 +3,30 @@ import "./card.styles.css";
 const Card = ({ subscriber }) => {
   const { id, user_name, founder, profile_photo } = subscriber;
   const isFounder = founder === "yes";
-  const founderClass = isFounder ? "founder" : "";
 
-  return (
-    <div key={id} className={`card-container ${founderClass}`}>
-      <img alt={`subscriber ${user_name}`} src={profile_photo} />
-      <h2 className="card-header">{user_name}</h2>
-      {isFounder && <p className="founder-text">Founder</p>}
-    </div>
-  );
+  
+  if (isFounder) {
+    return (
+      <div key={id} className={`card-container rgb founder`}>
+        <img alt={`subscriber ${user_name}`} src={profile_photo} />
+        <div className="card-text">
+          <h2 className="card-header">{user_name}</h2>
+          <div className="founder-text">
+            <p>Founder</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div key={id} className={`card-container`}>
+        <img alt={`subscriber ${user_name}`} src={profile_photo} />
+        <div className="card-text">
+          <h2 className="card-header">{user_name}</h2>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Card;
